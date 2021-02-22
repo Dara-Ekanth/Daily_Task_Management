2fe1be8c9d8fc6c2ca13a707af3b97173a6013da
Dara-Ekanth <daraekanth3@gmail.com>
2021-02-22 19:42:22 +0530

    New urls added.

--- Daily_task/accounts/url.py
+++ Daily_task/accounts/url.py
@@ -3,6 +3,8 @@ from . import views
 urlpatterns = [
     path('',views.home,name ='home'),
     path('reg/',views.reg_view,name="reg_page"),
+    path('login/',views.login_view,name="login_page"),
+    path('logout/',views.logout_user,name='logout'),
     path('create/task',views.create_task,name="create_task"),
     path('view/task/',views.view_tasks,name="task_view"),
     path('update/task/<str:pk>',views.update_task,name="update_task"),
